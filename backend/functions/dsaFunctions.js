19229c48cbf986dd3c98370d111d435ec2ad786b
TAPABRATA DUTTA <tapabratadutta777@gmail.com>
2026-04-21 13:26:35 +0530

    Check the branching

--- backend/functions/dsaFunctions.js
+++ backend/functions/dsaFunctions.js
@@ -1,3 +1,9 @@
  export const dsaFunction = (req, res) => {
     const {id_name } = req.params
+    if(!id_name){
+        return  res.json({
+            status: false,
+            message: "Fill the id name"
+        })
+    }
 }
